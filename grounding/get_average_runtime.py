ce76bf601c74f02fb2359c20d69f7dbddb96fb12
Irma Ravkic <irma.ravkic@gmail.com>
2018-01-23 16:46:39 -0800

    generate commands changed

--- grounding/get_average_runtime.py
+++ grounding/get_average_runtime.py
@@ -26,15 +26,15 @@ def get_average(csvs_to_merge,out):
 
     return average_time
 
-def merge_csvs_main(path_to_results,out,exp,r):
+def merge_csvs_main(path_to_results,out,exp):
     if not os.path.isdir(out):
         os.makedirs(out)
     csvs_to_merge_train = []
     csvs_to_merge_test = []
     for dir in os.listdir(path_to_results):
         if "fold" in dir:
-            csvs_to_merge_train.append(os.path.join(path_to_results, dir,r, exp, "merged_train_time.csv"))
-            csvs_to_merge_test.append(os.path.join(path_to_results, dir,r, exp, "merged_test_time.csv"))
+            csvs_to_merge_train.append(os.path.join(path_to_results, dir,exp, "merged_train_time.csv"))
+            csvs_to_merge_test.append(os.path.join(path_to_results, dir,exp, "merged_test_time.csv"))
     train_time=get_average(csvs_to_merge_train, os.path.join(out, "average_train_time.csv"))
     test_time =get_average(csvs_to_merge_test, os.path.join(out, "average_test_time.csv"))
     print "Average total time over folds (train):",train_time
@@ -44,7 +44,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run exhaustive approach')
     parser.add_argument('-p', help='path to train gpickle')
     parser.add_argument('-e', help='path to train gpickle')
-    parser.add_argument('-r', help='name of results folder')
     parser.add_argument('-o', help='output')
     args = parser.parse_args()
-    merge_csvs_main(args.p, args.o, args.e,args.r)
+    merge_csvs_main(args.p, args.o, args.e)
