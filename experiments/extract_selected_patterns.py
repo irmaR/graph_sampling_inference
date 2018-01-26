c617b64c4ca183d938c4ff9f8a4ba2a844862ff8
Irma Ravkic <irma.ravkic@gmail.com>
2018-01-26 12:17:24 -0800

    generate commands changed

---
+++ experiments/extract_selected_patterns.py
@@ -0,0 +1,18 @@
+import os,shutil,argparse
+
+
+def move_selected_patterns(selected_pattern_list,output_directory):
+    with open(selected_pattern_list,'r') as f:
+        for line in f:
+             pattern_file=os.path.join(line,'input_pattern.gml')
+             pattern_name=line.split("/")[-1]
+             print pattern_name
+             shutil.copy(pattern_file,os.path.join(output_directory,pattern_name,'.gml'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run exhaustive approach')
+    parser.add_argument('-pl', help='path to data file')
+    parser.add_argument('-o', help='output path')
+    args = parser.parse_args()
+    move_selected_patterns(args.pl, args.o)
