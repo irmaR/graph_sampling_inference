2c229f8578c637faf69a240068fed75e4ef87608
Irma Ravkic <irma.ravkic@gmail.com>
2018-01-26 12:19:47 -0800

    generate commands changed

--- experiments/extract_selected_patterns.py
+++ experiments/extract_selected_patterns.py
@@ -2,6 +2,8 @@ import os,shutil,argparse
 
 
 def move_selected_patterns(selected_pattern_list,output_directory):
+    if not os.path.exists(output_directory):
+     os.makedirs(output_directory)
     with open(selected_pattern_list,'r') as f:
         for line in f:
              pattern_file=os.path.join(line,'input_pattern.gml')
